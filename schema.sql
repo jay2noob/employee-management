f735183bb092eb4c6b25656eafdc88a0bf95bbcb
John Fields <jjfields24@outlook.com>
2020-02-14 10:34:31 -0500

    added mysql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE employeeTracker_DB;
+CREATE DATABASE employees_db;
 
-USE employeeTracker_DB;
+USE employees_db;
 
 CREATE TABLE department (
     id INT(10) AUTO_INCREMENT,
@@ -30,28 +30,32 @@ INSERT INTO department (name)
 VALUES ("Sales"),
 	   ("Engineering"),
 	   ("Accounting"),
-	   ("Legal"),
-	   ("Office");
+	   ("Legal");
 
 Select * FROM department;
 
 INSERT INTO roles (title, salary, department_id)
 
-VALUES ("Sales Lead", 150000, 2),
-	   ("Salesperson", 90000, 2),
-	   ("Lead Engineer", 175000, 1),
-	   ("Software Engineer", 125000, 1),
+VALUES ("Sales Lead", 150000, 1),
+	   ("Salesperson", 90000, 1),
+	   ("Lead Engineer", 175000, 2),
+	   ("Software Engineer", 125000, 2),
 	   ("Account Manager", 150000, 3),
-	   ("Accountant", 100000, 3000),
+	   ("Accountant", 100000, 3),
 	   ("Legal Team Lead", 200000, 4),
-	   ("Copy Machine Guy", 12000, 9);
+	   ("Lawyer", 120000, 4);
 
 Select * FROM roles;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 
-VALUES ("Jack", "Torrance", 3, 3),
-	   ("Leia", "Organa", 4, 3),
-	   ("Tony", "Montana", 1, null),
+VALUES ("John", "Deere", 1, 1),
+	   ("Ronald", "McDonald", 2, null),
+	   ("Barbara", "Walters", 3, 2),
+       ("Tim", "Horton", 4, null),
+       ("Hulk", "Hogan", 5, 3),
+       ("Baker", "Mayfield", 6, null),
+       ("Michael", "Jordan", 7, 4),
+       ("Elizabeth", "Warren", 8, null);
 
 Select * FROM employee;
